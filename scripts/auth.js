b30d42bfe5d8334a77488a55f7d34345fbdd2e0e
Dreamufor <azimeng520@gmail.com>
2019-04-01 10:26:40 +1300

    catchErr

--- scripts/auth.js
+++ scripts/auth.js
@@ -97,11 +97,15 @@ signupForm.addEventListener('submit', (e) => {
         const modal = document.querySelector('#modal-signup');
         M.Modal.getInstance(modal).close();
         signupForm.reset();
+        signupForm.querySelector('.error').innerHTML = '';
+
+    }).catch(err => {
+        signupForm.querySelector('.error').innerHTML = err.message;
     });   
 });
 
 //logout
-const logout = document.querySelector("#logout");
+const logout = document.querySelector("#logout"); 
 logout.addEventListener('click', (e)=> {
     e.preventDefault();
     auth.signOut();
@@ -124,6 +128,9 @@ loginForm.addEventListener('submit',(e) => {
         const modal = document.querySelector('#modal-login');
         M.Modal.getInstance(modal).close();
         loginForm.reset();
+        loginForm.querySelector('.error').innerHTML = '';
+    }).catch(err => {
+        loginForm.querySelector('.error').innerHTML = err.message;
     });
 });
 
